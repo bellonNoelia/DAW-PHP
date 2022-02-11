4544c17ac6ec8fee25d0ad62b6a3b3973e2166c9
noebl <58354965+bellonNoelia@users.noreply.github.com>
2022-02-11 17:33:32 +0100

    Update borrar.php

--- Tarea03/borrar.php
+++ Tarea03/borrar.php
@@ -1,4 +1,34 @@
 <?php
-//Llamamos a la conexión.
-require_once("conexion.php"); 
+if (isset($_GET['id'])) {
+    //Llamamos a la conexión.
+    require_once("conexion.php");
+    $conexionProyecto=new PDO($dsn,$user,$pass);
+
+    $resultado = false;
+
+
+    try {
+
+        $id = $_GET['id'];
+        $delete = ("DELETE FROM productos WHERE id =$id");
+
+        $sentencia = $conexionProyecto->prepare($delete);
+        $sentencia->execute();
+    } catch (PDOException $error) {
+        $resultado = true;
+        $error = $error->getMessage();
+    }
+
+    if (isset($resultado)) {
+
+        if ($resultado === false) {
+
+            echo " Producto : $id  borrado correctamente.
+            <button type='button'><a href='listado.php'>Volver</a></button>";
+        } else {
+            echo "Fallo al eliminar los datos. ERRO:$error 
+            <button type='button' ><a href='listado.php'>Volver</a></button>";
+        }
+    }
+}
 ?>
