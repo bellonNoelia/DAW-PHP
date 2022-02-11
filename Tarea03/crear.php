b22ae78f69cfdb6c83efe9be8a7353b50cceebb3
noebl <58354965+bellonNoelia@users.noreply.github.com>
2022-02-11 14:42:46 +0100

    Update crear.php

--- Tarea03/crear.php
+++ Tarea03/crear.php
@@ -1,47 +1,46 @@
 <?php
 
 if (isset($_POST['crear'])) {
-    $resultado = [
-      'error' => false,
-      'mensaje' => 'El  producto ' . $_POST['nombre'] . ' ha sido agregado con éxito' 
-    ];
-//Llamamos a la conexión.
-require_once("conexion.php"); 
-if(isset($_POST['crear'])){
-//Creamos variables de la información enviada
-$nombre=$_POST['nombre'];
-$nombreCorto=$_POST['nombreCorto'];
-$precio=$_POST['precio'];
-$familia=$_POST['familia'];
-$descripcion=$_POST['descripcion'];
+    $resultado = false;
+    //Llamamos a la conexión.
+    require_once("conexion.php");
 
-//Insercción de datos
-$insert="INSERT INTO productos(nombre,nombre_corto,descripcion,pvp,familia)
+    //Creamos variables de la información enviada
+    $nombre = $_POST['nombre'];
+    $nombreCorto = $_POST['nombreCorto'];
+    $precio = $_POST['precio'];
+    $familia = $_POST['familia'];
+    $descripcion = $_POST['descripcion'];
+
+    //Insercción de datos
+    $insert = "INSERT INTO productos(nombre,nombre_corto,descripcion,pvp,familia)
 VALUES(:nombre,:nombreCorto,:descripcion,:precio,:familia)";
-$stmt=$conexionProyecto->prepare($insert);
-try{
-    $stmt->execute([':nombre'=>$nombre,
-    ':nombreCorto'=>$nombreCorto,
-    ':descripcion'=>$descripcion,
-    ':precio'=>$precio,
-    ':familia'=>$familia]);
-}catch(PDOException $ex){
-    $resultado['error'] = true;
-    $resultado=$ex->getMessage();
+
+    $stmt = $conexionProyecto->prepare($insert);
+    try {
+        $stmt->execute([
+            ':nombre' => $nombre,
+            ':nombreCorto' => $nombreCorto,
+            ':descripcion' => $descripcion,
+            ':precio' => $precio,
+            ':familia' => $familia
+        ]);
+    } catch (PDOException $ex) {
+        $resultado = true;
+        $error = $ex->getMessage();
+    }
 }
+print_r($_POST);
+
 if (isset($resultado)) {
-    ?>
-<div class="container mt-3">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="alert alert-<?= $resultado['error'] ? 'danger' : 'success' ?>" role="alert">
-                <?= $resultado['mensaje'] ?>
-            </div>
-        </div>
-    </div>
-</div>
-<?php
-}}}
+
+    if ($resultado === false) {
+
+        echo "<div class='alert alert-success' role='alert' > Producto : $nombre  agregado correctamente.</div>";
+    } else {
+        echo "<div class='content alert alert-danger' role='alert'> No se pueden agregar datos. ERRO:$error </div>";
+    }
+}
 
 ?>
 
@@ -56,8 +55,7 @@ if (isset($resultado)) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS v5.0.2 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="estilos.css">
 
 
@@ -71,7 +69,7 @@ if (isset($resultado)) {
         </div>
 
         <div class="container mt-5">
-            <form method="POST" action="">
+            <form method="POST" >
                 <div class="row">
                     <div class="col">
                         <label for="nombre">Nombre</label>
@@ -79,8 +77,7 @@ if (isset($resultado)) {
                     </div>
                     <div class="col">
                         <label for="nombreCorto">Nombre Corto</label>
-                        <input id="nombreCorto" name="nombreCorto" type="text" class="form-control"
-                            placeholder="Nombre Corto">
+                        <input id="nombreCorto" name="nombreCorto" type="text" class="form-control" placeholder="Nombre Corto">
                     </div>
                 </div>
                 <div class="row">
@@ -116,13 +113,13 @@ if (isset($resultado)) {
                         <div class="row">
                             <div class="col">
 
-                                <button type="submit" class="btn btn-primary">Crear</button>
+                                <button type="submit"   name="crear" class="btn btn-primary">Crear</button>
                             </div>
                             <div class="col">
-                                <button type="submit" class="btn btn-success">Limpiar</button>
+                                <button type="reset" class="btn btn-success">Limpiar</button>
                             </div>
                             <div class="col">
-                                <button type="submit" class="btn btn-light"><a href="listado.php">Volver</a></button>
+                                <button type="button" class="btn btn-light"><a href="listado.php">Volver</a></button>
                             </div>
                         </div>
                     </div>
@@ -134,11 +131,9 @@ if (isset($resultado)) {
 
 
     <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
     </script>
 </body>
 
