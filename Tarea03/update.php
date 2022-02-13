e0352e24cef30e40252047d3cf74fe0ebe323220
noebl <58354965+bellonNoelia@users.noreply.github.com>
2022-02-13 14:20:23 +0100

    Update update.php

--- Tarea03/update.php
+++ Tarea03/update.php
@@ -1,61 +1,78 @@
 <?php
-if (isset($_GET['id'])) {
-    //Llamamos a la conexión.
-    require_once("conexion.php");
-    $conexionProyecto=new PDO($dsn,$user,$pass);
+//Si no mandamos el id nos lleva a listado.php
+if (!isset($_GET['id'])) {
+    header('Location:listado.php');
+}
 
-    if (isset($_POST['modificar'])) {
+//Llamamos a la conexión.
+require_once("conexion.php");
+$conexionProyecto = new PDO($dsn, $user, $pass);
+$familia = "SELECT cod,nombre FROM familias ORDER BY nombre";
+$consulta = "SELECT * FROM productos WHERE id=:i";
+$id = $_GET['id'];
+$stmt = $conexionProyecto->prepare($familia);
+$sentencia = $conexionProyecto->prepare($consulta);
+try {
+    $stmt->execute();
+    $sentencia->execute([':i=>$id']);
+} catch (PDOException $ex) {
+    $resultado = true;
+    $error = $ex->getMessage();
+    echo "<div class='content alert alert-danger' role='alert'> Error al recuperar el producto o la familia. ERROR:$error </div>";
+}
+$producto = $sentencia->fetch(PDO::FETCH_OBJ);
+if (isset($_GET['modificar'])) {
     try {
         $producto = [
             "id"        => $_GET['id'],
-            "nombre"    => $_POST['nombre'],
-            "nombreCorto "  => $_POST['nombreCorto '],
-            "descripcion"     => $_POST['descripcion'],
-            "precio"      => $_POST['precio']
+            "nombre"    => $_GET['nombre'],
+            "nombreCorto "  => $_GET['nombre_corto'],
+            "descripcion"     => $_GET['descripcion'],
+            "precio"      => $_GET['pvp'],
+            "familia"     => $_GET['familia']
         ];
 
-        $update= "UPDATE productos SET
-    nombre = :nombre,
-    nombreCorto = :nombreCorto,
-    descripcion = :descripcion,
-    precio = :precio,
-    familia = :familia,
-    updated_at = NOW()
-    WHERE id = :id";
+        $update = "UPDATE productos SET
+            nombre = :nombre,
+            nombreCorto = :nombre_corto,
+            descripcion = :descripcion,
+            precio = :pvp,
+            familia = :familia,
+            WHERE id = :id";
 
         $sentencia = $conexionProyecto->prepare($update);
-        $sentencia->execute($producto);
-        $sentencia->execute();
+    } catch (PDOException $error) {
+        $resultado = true;
+        $error = $error->getMessage();
+    }
+    try {
+        $sentencia->execute([
+            ':nombre' => $nombre,
+            ':nombre_corto' => $nombreCorto,
+            ':descripcion' => $descripcion,
+            ':pvp' => $precio,
+            ':familia' => $familia
+        ]);
     } catch (PDOException $error) {
         $resultado = true;
         $error = $error->getMessage();
     }
 
-}
-    try {
-       
-          
-        $id = $_GET['id'];
-        $consultaSQL = "SELECT * FROM productos WHERE id=$id";
-      
-        $sentencia = $conexionProyecto->prepare($consultaSQL);
-        $sentencia->execute();
-      
-        $producto = $sentencia->fetch(PDO::FETCH_ASSOC);
-    } catch (PDOException $ex) {
-        $resultado = true;
-        $error = $ex->getMessage();
-    }
-          
-if (isset($resultado)) {
 
-    if ($resultado === false) {
 
-        echo "<div class='alert alert-success' role='alert' > Producto : $nombre  actualizado correctamente.</div>";
-    } else {
-        echo "<div class='content alert alert-danger' role='alert'> No se ha encontrado el producto. ERRO:$error </div>";
+    if (isset($resultado)) {
+
+        if ($resultado === false) {
+
+            echo "<div class='alert alert-success' role='alert' > Producto : $nombre  actualizado correctamente.</div>";
+        } else {
+            echo "<div class='content alert alert-danger' role='alert'> No se ha encontrado el producto. ERRO:$error </div>";
+        }
     }
 }
+
+
+
 
 
 ?>
@@ -70,8 +87,7 @@ if (isset($resultado)) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS v5.0.2 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="estilos.css">
 
 
@@ -81,77 +97,73 @@ if (isset($resultado)) {
 <body>
     <div class="container-fluid">
         <div>
-            <h3 class="text-center">Crear Producto</h3>
+            <h3 class="text-center">Modificar Producto</h3>
         </div>
+        <?php
+        //Recorremos el array 
+        foreach ($producto as $dato) {
+        ?>
+            <div class="container mt-5">
+                <form method="GET">
+                    <div class="row">
+                        <div class="col">
+                            <label for="nombre">Nombre</label>
+                            <input value="<? echo $dato->nombre ?>" id="nombre" name="nombre" type="text" class="form-control" placeholder="Nombre">
+                        </div>
+                        <div class="col">
+                            <label for="nombreCorto">Nombre Corto</label>
+                            <input value="<? echo $dato->nombre_corto ?> " id="nombreCorto" name="nombreCorto" type="text" class="form-control" placeholder="Nombre Corto">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <label for="precio">Precio (€)</label>
+                            <input value="<? echo $dato->pvp ?>" id="precio" name="precio" type="text" class="form-control" placeholder="Precio (€)">
+                        </div>
+                        <div class="col">
+                            <label for="familia">Familia</label>
+                            <select id="familia" name="familia" class="form-control">
+                                <?php
+                                while ($filas = $stmt->fetch(PDO::FETCH_OBJ)) {
+                                    if ($filas->cod == $producto->familia) {
+                                        echo "<option value='{$filas->cod}' selected>$filas->nombre</option>";
+                                    } else {
+                                        echo "<option value='{$filas->cod}'>$filas->nombre</option>";
+                                    }
+                                }
+                                ?>
 
-        <div class="container mt-5">
-            <form method="POST">
-                <div class="row">
-                    <div class="col">
-                        <label for="nombre">Nombre</label>
-                        <input value="<?=($producto['nombre']) ?>" id="nombre" name="nombre" type="text"
-                            class="form-control" placeholder="Nombre">
-                    </div>
-                    <div class="col">
-                        <label for="nombreCorto">Nombre Corto</label>
-                        <input value="<?= ($producto['nombreCorto']) ?> " id="nombreCorto" name="nombreCorto"
-                            type="text" class="form-control" placeholder="Nombre Corto">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <label for="precio">Precio (€)</label>
-                        <input value="<?= ($producto['precio']) ?>" id="precio" name="precio" type="text"
-                            class="form-control" placeholder="Precio (€)">
-                    </div>
-                    <div class="col">
-                        <label for="familia">Familia</label>
-                        <select value="<?= ($producto['familia']) ?>" id="familia" name="familia" class="form-control">
-                            <option value="CAMARA">Cámaras digitales</option>
-                            <option value="CONSOL">Consolas</option>
-                            <option value="EBOOK">Libros electrónicos</option>
-                            <option value="IMPRES">Impresoras</option>
-                            <option value="MEMFLA">Memorias flash</option>
-                            <option value="MP3">Reproductores MP3</option>
-                            <option value="MULTIF">Equipos multifunción</option>
-                            <option value="NETBOK">Netbooks</option>
-                            <option value="ORDENA">Ordenadores</option>
-                            <option value="PORTAT">Ordenadores portátiles</option>
-                            <option value="ROUTER">Routers</option>
-                            <option value="SAI">Sistemas de alimentación initerrumpida</option>
-                            <option value="SOFTWA">Software </option>
-                            <option value="TV">Televisores</option>
-                            <option value="VIDEOC">Videocámaras</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label value="<?= ($producto['descripcion']) ?>" for="descripcion">Descripción</label>
-                        <textarea class="form-control" name="descripcion" id="descripcion" rows="12"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col">
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label value="<? echo $dato->descripcion ?>" for="descripcion">Descripción</label>
+                            <textarea class="form-control" name="descripcion" id="descripcion" rows="12"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col">
 
-                                <button type="submit" name="modificar" class="btn btn-primary">Modificar</button>
-                            </div>
-                            <div class="col">
-                                <button type="button" class="btn btn-info"><a href="listado.php">Volver</a></button>
+                                    <button type="submit" name="modificar" class="btn btn-primary">Modificar</button>
+                                </div>
+                                <div class="col">
+                                    <button type="button" class="btn btn-info"><a href="listado.php">Volver</a></button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </form>
-        </div>
+                </form>
+            </div>
+        <?php
+        }
+        ?>
     </div>
 
 
 
     <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
     </script>
 </body>
 
