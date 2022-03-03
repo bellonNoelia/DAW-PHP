c841675030bca450ca3ba8b32255f10f61e327fe
noebl <58354965+bellonNoelia@users.noreply.github.com>
2022-03-03 13:02:26 +0100

    Update Jugador.php

--- Tarea05/Jugador.php
+++ Tarea05/Jugador.php
@@ -1,6 +1,11 @@
 <?php
 
-class Jugador{
+use Clases\Conexion;
+use PDO;
+use PDOException;
+
+class Jugador  extends Conexion
+{
 
     private $nombre;
     private $apellidos;
@@ -8,21 +13,61 @@ class Jugador{
     private $numeroDorsal;
     private $codigoBarras;
 
-    function __construct($nombre,$apellidos,$posicion,$numeroDorsal,$codigoBarras)
+    function __construct($nombre, $apellidos, $posicion, $numeroDorsal, $codigoBarras)
+    {
+        parent::__construct();
+    }
+
+    public function setNombre($nombre)
     {
         $this->nombre = $nombre;
-        $this->apellidos = $apellidos;
-        $this->posicion = $posicion;
-        $this->numeroDorsal = $numeroDorsal;
-        $this->codigoBarras = $codigoBarras;
-        
-
     }
-
-    function __set($name, $value)
+    public function setApellidos($apellidos)
     {
-        
+        $this->apellidos = $apellidos;
+    }
+    public function setPosicion($posicion)
+    {
+        $this->posicion = $posicion;
+    }
+    public function setDorsal($numeroDorsal)
+    {
+        $this->numeroDorsal = $numeroDorsal;
+    }
+    public function setCodigo($codigoBarras)
+    {
+        $this->codigoBarras = $codigoBarras;
     }
 
+    //Función para crear jugadores:
+    function create()
+    {
+        $insert = "INSERT INTO practicaUnidad5(nombre,apellidos,dorsal,posicion,barcode)
+        VALUES (:n,:a,:d,:p,:b)";
+        $stmt = $this->conexion->prepare($insert);
+        try {
+            $stmt->execute([
+                ':n' => $this->nombre,
+                ':a' => $this->apellidos,
+                ':d' => $this->numeroDorsal,
+                ':p' => $this->posicion,
+                ':b' => $this->codigoBarras
+            ]);
+        } catch (PDOException $ex) {
+            die("Ocurrio un error al insertar el jugador: " . $ex->getMessage());
+        }
+    }
+    //Función para leer los datos de un jugador:
+    function read()
+    {
+        $consulta = "SELECT * FROM practicaUnidad5 WHERE id=:i";
+        $stmt = $this->conexion->prepare($consulta);
+        try {
+            $stmt->execute([':i' => $this->id]);
+        } catch (PDOException $ex) {
+            die("Error al recuperar el jugador: " . $ex->getMessage());
+        }
+        return $stmt->fetchAll(PDO::FETCH_OBJ); //Devolvemos con All sólo es una fila
+    }
 }
 ?>
