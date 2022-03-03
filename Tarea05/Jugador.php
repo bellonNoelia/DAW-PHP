c1b4a12a0cbf9a21f33e200b3d053ea22cadb743
noebl <58354965+bellonNoelia@users.noreply.github.com>
2022-03-03 15:02:40 +0100

    Update Jugador.php

--- Tarea05/Jugador.php
+++ Tarea05/Jugador.php
@@ -10,11 +10,10 @@ class Jugador  extends Conexion
     private $nombre;
     private $apellidos;
     private $posicion;
-    private $numeroDorsal;
-    private $codigoBarras;
+    private $dorsal;
+    private $barcode;
 
-    function __construct($nombre, $apellidos, $posicion, $numeroDorsal, $codigoBarras)
-    {
+    function __construct(){
         parent::__construct();
     }
 
@@ -30,37 +29,37 @@ class Jugador  extends Conexion
     {
         $this->posicion = $posicion;
     }
-    public function setDorsal($numeroDorsal)
+    public function setDorsal($dorsal)
     {
-        $this->numeroDorsal = $numeroDorsal;
+        $this->dorsal = $dorsal;
     }
-    public function setCodigo($codigoBarras)
+    public function setBarcode($barcode)
     {
-        $this->codigoBarras = $codigoBarras;
+        $this->barcode = $barcode;
     }
 
     //Función para crear jugadores:
     function create()
     {
-        $insert = "INSERT INTO practicaUnidad5(nombre,apellidos,dorsal,posicion,barcode)
+        $insert = "INSERT INTO jugadores(nombre,apellidos,dorsal,posicion,barcode)
         VALUES (:n,:a,:d,:p,:b)";
         $stmt = $this->conexion->prepare($insert);
         try {
             $stmt->execute([
                 ':n' => $this->nombre,
                 ':a' => $this->apellidos,
-                ':d' => $this->numeroDorsal,
+                ':d' => $this->dorsal,
                 ':p' => $this->posicion,
-                ':b' => $this->codigoBarras
+                ':b' => $this->barcode
             ]);
         } catch (PDOException $ex) {
-            die("Ocurrio un error al insertar el jugador: " . $ex->getMessage());
+            die("Ocurrió un error al insertar el jugador: " . $ex->getMessage());
         }
     }
     //Función para leer los datos de un jugador:
     function read()
     {
-        $consulta = "SELECT * FROM practicaUnidad5 WHERE id=:i";
+        $consulta = "SELECT * FROM jugadores WHERE id=:i";
         $stmt = $this->conexion->prepare($consulta);
         try {
             $stmt->execute([':i' => $this->id]);
@@ -69,5 +68,103 @@ class Jugador  extends Conexion
         }
         return $stmt->fetchAll(PDO::FETCH_OBJ); //Devolvemos con All sólo es una fila
     }
+
+    //Función para actualizar datos:
+    function update(){
+        $update="UPDATE jugadores SET nombre=:n,apellidos=:a,dorsal:d,posicion:p,barcode:b WHERE id=:i";
+        $stmt = $this->conexion->prepare($update);
+        try {
+            $stmt->execute([
+                ':i'=>$this->id,
+                ':n' => $this->nombre,
+                ':a' => $this->apellidos,
+                ':d' => $this->dorsal,
+                ':p' => $this->posicion,
+                ':b' => $this->barcode
+            ]);
+        } catch (PDOException $ex) {
+            die("Ocurrió un error al actualizar el jugador: " . $ex->getMessage());
+        }
+    }
+
+    //Función para borrar un jugador:
+    function delete(){
+        $delete="DELETE FROM jugadores WHERE id=:id";
+        $stmt = $this->conexion->prepare($delete);
+        try {
+            $stmt->execute([':i' => $this->id]);
+        } catch (PDOException $ex) {
+            die("Error al borrar el jugador: " . $ex->getMessage());
+        }
+    }
+
+    //Función para obtener todos los jugadores:
+    function listadoJugadores(){
+        $consulta="SELECT * FROM jugadores ORDER BY nombre";
+        $stmt = $this->conexion->prepare($consulta);
+        try{
+            $stmt->execute();
+        }catch(PDOException $ex){
+            die("Error al recuperar jugadores: " . $ex->getMessage());
+        }
+        return $stmt;
+    }
+
+    //Función para comprobar que el barcode ya existe
+    function barcodeExiste($barc){
+        //Para crear:
+        if($this->id==null){
+            $consulta="SELECT * FROM jugadores WHERE barcode:b";
+        }else{
+            //Para modificar:
+                $consulta="SELECT * FROM jugadores WHERE barcode:b AND id!=:i";
+        }
+        $stmt=$this->conexion->prepare($consulta);
+        try{
+            //Para crear:
+            if($this->id==null){
+                $stmt->execute([':b'=>$barc]);
+            }else{
+                //Para modificar:
+                $stmt->execute([':b'=>$barc,':i'=>$this->id]);
+            }
+        }catch(PDOException $ex){
+            die("Error al comprobar barcode: " . $ex->getMessage());
+        }
+
+        if($stmt->rowCount()==0){
+            return false; //No existe
+        }else{
+            return true; //Existe
+        }
+    }
+
+    //Función para comprobar que el dorsal ya existe
+    function dorsalExiste($dors){
+        //Para crear:
+        if($this->id==null){
+            $consulta="SELECT * FROM jugadores WHERE dorsal:d";
+        }else{
+            //Para modificar:
+                $consulta="SELECT * FROM jugadores WHERE dorsal:d AND id!=:i";
+        }
+        $stmt=$this->conexion->prepare($consulta);
+        try{
+            //Para crear:
+            if($this->id==null){
+                $stmt->execute([':b'=>$dors]);
+            }else{
+                //Para modificar:
+                $stmt->execute([':b'=>$dors,':i'=>$this->id]);
+            }
+        }catch(PDOException $ex){
+            die("Error al comprobar dorsal: " . $ex->getMessage());
+        }
+
+        if($stmt->rowCount()==0){
+            return false; //No existe
+        }else{
+            return true; //Existe
+        }
+    }
 }
-?>
