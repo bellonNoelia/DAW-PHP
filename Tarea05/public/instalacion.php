0d4428209c9647e0de0a39db96a94b1d54cea503
noebl <58354965+bellonNoelia@users.noreply.github.com>
2022-03-06 17:50:03 +0100

    Update instalacion.php

--- Tarea05/public/instalacion.php
+++ Tarea05/public/instalacion.php
@@ -1,5 +1,13 @@
 <?php
 require '../vendor/autoload.php';
 
-use Clases\Jugador;
 use Philo\Blade\Blade;
+
+$views = '../views';
+$cache = '../cache';
+$blade = new Blade($views, $cache);
+
+echo $blade
+        ->view()
+        ->make('vinstalacion')
+        ->render();
