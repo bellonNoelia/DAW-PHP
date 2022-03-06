b5fe7cac21ac1150be849c8006d9b0d84597fd0f
noebl <58354965+bellonNoelia@users.noreply.github.com>
2022-03-06 20:39:05 +0100

    Update index.php

--- Tarea05/public/index.php
+++ Tarea05/public/index.php
@@ -4,13 +4,15 @@ require '../vendor/autoload.php';
 use Clases\Jugador;
 
 $jugador = new Jugador();
-$consulta = $this->conexion->query("SELECT * FROM jugadores");
-echo $consulta;
-if($consulta->num_rows > 0){
+
+if(!$jugador->tablaVacia){
         header('Location:jugadores.php');
         exit();
 }else{
         header('Location:instalacion.php');
     exit();
 }
+
+
+
 
