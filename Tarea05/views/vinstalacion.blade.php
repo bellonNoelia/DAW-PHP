4163d52c7afaab977ec63e4d026ee5e425b9e6cf
noebl <58354965+bellonNoelia@users.noreply.github.com>
2022-03-06 17:52:45 +0100

    Update vinstalacion.blade.php

--- Tarea05/views/vinstalacion.blade.php
+++ Tarea05/views/vinstalacion.blade.php
@@ -14,7 +14,7 @@
 
     <div class="container mt-5" >
     <div  style="text-align: center;">
-        <button type="submit" class="btn btn-success" style="background-color:   #3db215  ; border-color:  #3db215 ; font-size: x-large;"><a href="crearDatos.php" style="color: #ffffff;appearance: button;text-decoration: none;"> <i class="fa fa-database"></i></i> Instalar Datos de Ejemplo</a></button>
+        <button type="submit" class="btn btn-success" style="background-color:   #3db215  ; border-color:  #3db215 ; font-size: x-large;"><a href="crearDatos.php" style="color: #ffffff;appearance: button;text-decoration: none;"> <i class="fa fa-database"></i> Instalar Datos de Ejemplo</a></button>
         </div>
     </div>
 
