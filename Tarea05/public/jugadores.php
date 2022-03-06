14b4ecd2ec6eeaf79de0db6618bb60de5c50c62c
noebl <58354965+bellonNoelia@users.noreply.github.com>
2022-03-06 17:50:20 +0100

    Update jugadores.php

--- Tarea05/public/jugadores.php
+++ Tarea05/public/jugadores.php
@@ -3,3 +3,14 @@ require '../vendor/autoload.php';
 
 use Clases\Jugador;
 use Philo\Blade\Blade;
+
+$views = '../views';
+$cache = '../cache';
+$blade = new Blade($views, $cache);
+
+
+$jugadores = (new Jugador())->listadoJugadores();
+echo $blade
+        ->view()
+        ->make('vjugadores',compact( 'jugadores'))
+        ->render();
