d983148a8f48ea9129a7a8365ca58fd424b91d68
noebl <58354965+bellonNoelia@users.noreply.github.com>
2022-03-06 17:49:00 +0100

    Update index.php

--- Tarea05/public/index.php
+++ Tarea05/public/index.php
@@ -2,8 +2,15 @@
 require '../vendor/autoload.php';
 
 use Clases\Jugador;
-use Philo\Blade\Blade;
 
-$views = '../views';
-$cache = '../cache';
-$blade = new Blade($views, $cache);
+$jugador = new Jugador();
+$consulta = $this->conexion->query("SELECT * FROM jugadores");
+echo $consulta;
+if($consulta->num_rows > 0){
+        header('Location:jugadores.php');
+        exit();
+}else{
+        header('Location:instalacion.php');
+    exit();
+}
+
