589806640451f2adeb80b56d5a8b460883253342
noebl <58354965+bellonNoelia@users.noreply.github.com>
2022-04-21 15:41:09 +0200

    Update cliente.php

--- DWES06/public/cliente.php
+++ DWES06/public/cliente.php
@@ -1,7 +1,7 @@
 <?php
 require '../src/Operaciones.php';
-$url = 'http://127.0.0.1/~noeliabellon/3%c2%ba%20EVAL/DWES06/servidorSoap/servicio.php';
-$uri = 'http://127.0.0.1/~noeliabellon/3%c2%ba%20EVAL/DWES06/servidorSoap';
+$url = 'http://127.0.0.1/~noeliabellon/3EVAL/DWES06/servidorSoap/servicio.php';
+$uri = 'http://127.0.0.1/~noeliabellon/3EVAL/DWES06/servidorSoap';
 $codigoProducto=14;
 $codigoTienda=1;
 $codigoFamilia='CAMARA';
