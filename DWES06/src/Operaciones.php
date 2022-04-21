344d0b24577ce6d35a562f56cf8e3abdce2dae4f
noebl <58354965+bellonNoelia@users.noreply.github.com>
2022-04-21 15:12:25 +0200

    Update Operaciones.php

--- DWES06/src/Operaciones.php
+++ DWES06/src/Operaciones.php
@@ -2,19 +2,40 @@
 
 namespace Clases;
 
-class Operaciones 
+class Operaciones
 {
-    function getPvp(){
-
+    function getPvp($id)
+    {
+        $producto = new Producto();
+        $producto->setId($id);
+        $pvp = $producto->pvpProducto();
+        return $pvp;
+        $producto = null;
     }
 
-    function getStock(){
-        
+    function getStock($idP, $tienda)
+    {
+        $stock = new Stock();
+        $stock->setProducto($idP);
+        $stock->setTienda($tienda);
+        $stockProducto = $stock->stock();
+        return $stockProducto;
+        $stock = null;
     }
-    function getFamilias(){
-        
+
+    function getFamilias()
+    {
+        $familia = new Familia();
+        $codFamilia = $familia->mostrarFamilias();
+        return $codFamilia;
+        $familia = null;
     }
-    function getProdutos(){
-        
+
+    function getProdutos($familia)
+    {
+        $producto = new Producto();
+        $famProducto = $producto->familiaProducto($familia);
+        return $famProducto;
+        $producto = null;
     }
 }
