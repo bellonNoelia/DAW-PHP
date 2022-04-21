564079681be2710b6701450833db20870075ad7d
noebl <58354965+bellonNoelia@users.noreply.github.com>
2022-04-21 15:33:21 +0200

    Update cliente.php

--- DWES06/public/cliente.php
+++ DWES06/public/cliente.php
@@ -11,11 +11,28 @@ try {
     echo "Error: ".$ex->getMessage();
 }
 $pvp=$cliente->getPVP($codigoProducto);
+echo "<ul>";
+    echo "<li><code>$pvp</code></li>";
+echo "</ul>";
 
 $stock=$cliente->getStock($codigoProducto,$codigoTienda);
+echo "<ul>";
+    echo "<li><code>$stock</code></li>";
+echo "</ul>";
+
 
 $familia=$cliente->getFamilia();
+echo "<ul>";
+foreach ($familia as $k => $v) {
+    echo "<li><code>$v</code></li>";
+}
+echo "</ul>";
 
 $productoFamilia=$cliente->getProducto($codigoFamilia);
+echo "<ul>";
+foreach ($productoFamilia as $k => $v) {
+    echo "<li><code>$v</code></li>";
+}
+echo "</ul>";
 ?>
 
