bf75505619dc685bfe5f934f4652276568d297e5
noebl <58354965+bellonNoelia@users.noreply.github.com>
2022-04-21 15:40:41 +0200

    Update servicio.php

--- DWES06/servidorSoap/servicio.php
+++ DWES06/servidorSoap/servicio.php
@@ -1,6 +1,6 @@
 <?php
 require '../src/Operaciones.php';
-$uri='http://127.0.0.1/~noeliabellon/3%c2%ba%20EVAL/DWES06/servidorSoap';
+$uri='http://127.0.0.1/~noeliabellon/3EVAL/DWES06/servidorSoap;
 try {
     $server = new SoapServer(NULL,['uri'=>$uri]);
     $server->setClass('Operaciones');
