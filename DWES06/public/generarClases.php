30ee51bd44a491aee06fce80cf2b8acf086bbd61
noebl <58354965+bellonNoelia@users.noreply.github.com>
2022-04-21 17:17:44 +0200

    Add files via upload

---
+++ DWES06/public/generarClases.php
@@ -0,0 +1,17 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use Wsdl2PhpGenerator\Generator;
+use Wsdl2PhpGenerator\Config;
+
+$generator =new Generator();
+$generator->generate(
+    new Config([
+        'inputFile' => "http://127.0.0.1/~noeliabellon/3EVAL/DWES06/servidorSoap?wsdl", //wsdl
+        'outputDir' => '../src/Clases1',  //directorio donde vamos a generar las clases
+        'namespaceName' => 'Clases'  //namespace que vamos a usar con ellas (lo indicamos en composer)
+    ])
+    );
+
+?>
